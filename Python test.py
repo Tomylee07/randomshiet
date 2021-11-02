fc5f54339c0cacd3232cec845e999a63508e3fe8
Tomylee07 <tomy.tom.07.10@gmail.com>
2021-11-02 14:56:15 -0300

    Inputest

--- Python test.py
+++ Python test.py
@@ -1,5 +1,13 @@
-x=30
-if x >= 30:
-    print('{0} es igual a 30'.format(x))
-else:
-    print(f'x es mayor')
+bienv = input('¿Cunato es 2 + 2?\n')
+
+while bienv != '4':
+    print('Nop')
+    bienv = input('Desea realizar otro intento? [Y/N]\n')
+    if bienv in 'Nn':
+        print('cya')
+        break
+    else:
+        print('lo tomare como un si')
+    bienv = input('¿Cuanto es 2 + 2?\n')
+if bienv == '4':
+    print('buena pa')
